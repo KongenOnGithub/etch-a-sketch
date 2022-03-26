8b3715c7d8b1865d42dcfd370cae22852c4ac939
KongenOnGithub <michaeldj100@gmail.com>
2022-03-26 16:10:19 +0100

    Add new grid button with needed functions
    functions added removeAllChildNodes, newGrid and createGrid

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('.newbtn')
+button.addEventListener('click',newGrid);
+
+function newGrid() {
+    removeAllChildNodes(container);
+    let gridSize= parseInt(prompt("what grid size would you like? you can't go over 100"));
+    if(gridSize>100){
+      gridSize= 100;
+    }
+    createGrid(gridSize);
+}
+
+function createGrid(size){
+    for (i=0; i<size; i++) {
+        for (j=0; j<size; j++) {
+          const newDiv = document.createElement('div');
+          newDiv.classList.add(`square`);
+          
+          let height= (960/size);
+          newDiv.style.cssText=`height: ${height}px;`
+          container.appendChild(newDiv);
+          newDiv.addEventListener('mouseover', function(event){
+              event.target.classList.add("hovered");
+            } );
+          newDiv.addEventListener('mouseout', function(event){
+              event.target.classList.remove("hovered")
+            });
+          
+        }
+    }
+}
 
 for (i=0; i<16; i++) {
     for (j=0; j<16; j++) {
@@ -19,7 +50,9 @@ for (i=0; i<16; i++) {
     }
 }
 
-function hoverChange(e) {
-    e.style.cssText='background-color: red;';
-    container.appendChild(e);
+
+function removeAllChildNodes(parent) {
+   while(parent.firstChild) {
+       parent.removeChild(parent.firstChild);
+   }
 }
